ad0982838fda50e477292cbffb31edcbf13144d5
Akash-Kumar-2 <yka334@gmail.com>
2024-05-27 19:13:42 +0530

    Adding Mail server and related routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,7 @@ const authController = require('./../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
+router.get("/verifyemail/:Code",authController.verifyEmailHandler);
 router.post('/login', authController.login);
 router.get('/logout',authController.logout);
 router.post('/forgetpassword', authController.forgetPassword);
--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,34 +1,42 @@
+
 const nodemailer = require('nodemailer');
 
-  //1) create a transporter
-  const transporter = nodemailer.createTransport({
-    service:'gmail',
-    // host: process.env.EMAIL_HOST,
-    // port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_FROM,
-      pass: process.env.EMAIL_PASSWORD
-    }
-    //disable in gmail "less secure app" option
-  });
 
-const sendEmail = async options => {
-try
-  {//2)define some options for email
-  const mailOptions = {
-    from: process.env.EMAIL_FROM,
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-    // html:
-  };
+// 1) create a transporter
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+});
 
-   // Actually send the email
-   await transporter.sendMail(mailOptions);
-   console.log('Mail Sent');
-}catch( err) {
-    console.log('Unable To send mail', err);
-}
+
+
+const sendEmail = async (To, subject, message) => {
+  try {
+    // 2) define some options for email
+const mailOptions = {
+  from: { 
+    name: 'Social Server',
+    address: process.env.EMAIL
+  },
+  to: To,
+  subject: subject,
+  text: message
+  // html:
 };
+
+    // Actually send the email
+    await transporter.sendMail(mailOptions);
+    console.log('Mail Sent');
+  } catch (err) {
+    console.log('Unable to send mail', err);
+  }
+};
+
 
 module.exports = sendEmail;
